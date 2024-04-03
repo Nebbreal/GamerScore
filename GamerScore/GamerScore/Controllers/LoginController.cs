3543926be17ae4f19e14870f5d03887a02e5a503
Demiën van Laarhoven <demien401@gmail.com>
2024-04-03 14:37:49 +0200

    Header created, Images imported & Started on login page

---
+++ GamerScore/GamerScore/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamerScore.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}

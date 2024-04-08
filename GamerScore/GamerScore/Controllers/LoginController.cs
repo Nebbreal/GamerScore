b8fc4d9b7298dd0410c77947c3c35e3a6726992f
Demiën van Laarhoven <demien401@gmail.com>
2024-04-08 14:31:42 +0200

    Created the front-end for Login, SignUp and Password reset screen
    Created the front-end for Login, SignUp and Password reset screen + minor fixes such a renaming index to home.

--- GamerScore/GamerScore/Controllers/LoginController.cs
+++ GamerScore/GamerScore/Controllers/LoginController.cs
@@ -9,7 +9,30 @@ namespace GamerScore.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string _email, string _password)
+        { 
+            return View(); 
+        }
+
         public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(string _email, string _username, string _password)
+        {
+            return View();
+        }
+
+        public IActionResult PasswordReset()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        IActionResult PasswordReset(string _email)
         {
             return View();
         }

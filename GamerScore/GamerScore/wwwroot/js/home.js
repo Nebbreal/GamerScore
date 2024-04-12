7e75be14685dddb0244f228047082725e82ca5b3
Demiën van Laarhoven <demien401@gmail.com>
2024-04-12 15:46:01 +0200

    Continued working on home page front-end + various fixes
    The css and html now get imported correctly, also added scroll functionality to the itemList

---
+++ GamerScore/GamerScore/wwwroot/js/home.js
@@ -0,0 +1,17 @@
+﻿function ScrollLeft() {
+    let itemlist = document.getElementById("newlyAddedItemList");
+    let scrollPosition = itemlist.scrollLeft;
+    itemlist.scroll({
+        left: scrollPosition - remToPx(81),
+        behavior: "smooth",
+    });
+}
+
+function ScrollRight() {
+    let itemlist = document.getElementById("newlyAddedItemList");
+    let scrollPosition = itemlist.scrollLeft;
+    itemlist.scroll({
+        left: scrollPosition + remToPx(81),
+        behavior: "smooth",
+    });
+}

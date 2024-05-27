3d432fa1327664abfbcbe130b69f2ce75b0b1c80
Demiën van Laarhoven <demien401@gmail.com>
2024-05-27 16:44:38 +0200

    front-end create review

--- GamerScore/GamerScore/wwwroot/js/Game.js
+++ GamerScore/GamerScore/wwwroot/js/Game.js
@@ -1,1 +1,15 @@
-﻿
+﻿function showCreateReview() {
+    var backgroundFade = document.getElementById("fade-bg");
+    var reviewContainer = document.getElementById("review-container");
+
+    reviewContainer.style.display = "flex";
+    backgroundFade.style.display = "block";
+}
+
+function closeReview() {
+    var backgroundFade = document.getElementById("fade-bg");
+    var reviewContainer = document.getElementById("review-container");
+
+    reviewContainer.style.display = "none";
+    backgroundFade.style.display = "none";
+}
